9b11961b048b49815f2d1172b03efd392727e415
aeccue <develop@aeccue.com>
2020-09-25 00:14:49 +0900

    Created DaggerInjector
    This injector is injected with the mapping of
    BaseDaggerSubcomponent.Factories that are provided in the dagger
    component. It will attempt to inject a target by looking for the factory
    using the class of the target, and if it exists, create the subcomponent
    and inject the target.

---
+++ dagger/core/src/main/kotlin/com/aeccue/foundation/dagger/injector/DaggerInjector.kt
@@ -0,0 +1,22 @@
+package com.aeccue.foundation.dagger.injector
+
+import com.aeccue.foundation.dagger.component.BaseDaggerSubcomponent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DaggerInjector
+@Inject constructor(factories: Set<@JvmSuppressWildcards BaseDaggerSubcomponent.Factory<Any, BaseDaggerSubcomponent<Any>>>) {
+
+    private val factoryMappings = factories.associateBy {
+        it.key
+    }
+
+    fun inject(target: Any): Boolean {
+        val factory = factoryMappings[target::class.java] ?: return false
+        (target as? Injectable)?.preInject()
+        factory.create(target).inject(target)
+        (target as? Injectable)?.postInject()
+        return true
+    }
+}
