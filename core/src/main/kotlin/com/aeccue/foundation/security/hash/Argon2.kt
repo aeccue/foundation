b0d7ed61e65174fd4e6f52f69a59f00d4bc79f1f
aeccue <develop@aeccue.com>
2020-09-29 23:43:58 +0900

    Created hashers for Argon2 and HMAC

---
+++ core/src/main/kotlin/com/aeccue/foundation/security/hash/Argon2.kt
@@ -0,0 +1,51 @@
+package com.aeccue.foundation.security.hash
+
+import de.mkammerer.argon2.Argon2
+import de.mkammerer.argon2.Argon2Factory
+
+private const val HASH_ITERATIONS = 2
+private const val HASH_MEMORY = 65536
+private const val HASH_PARALLELISM = 2
+
+/**
+ * Hashes passwords using Argon2 key derivation function and verify Argon2 hashes.
+ */
+object Argon2 {
+
+    /**
+     * Argon2id instance.
+     */
+    private val argon: Argon2 by lazy { Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id) }
+
+    /**
+     * Hashes a password with Argon2id.
+     *
+     * Default Configurations
+     * - iterations: 2
+     * - memory: 65536
+     * - parallelism: 2
+     *
+     * @param [password] The password to hash.
+     * @param [iterations] The number of iterations to hash the password. Defaults to 2.
+     * @param [memory] The amount of memory to use in kilobytes. Defaults to 65536.
+     * @param [parallelism] The number of threads and compute lanes to use. Defaults to 2.
+     * @return The hash of the password.
+     */
+    fun hash(password: String,
+             iterations: Int = HASH_ITERATIONS,
+             memory: Int = HASH_MEMORY,
+             parallelism: Int = HASH_PARALLELISM): String =
+            argon.hash(iterations, memory, parallelism, password.toCharArray())
+
+    /**
+     * Verifies a password against a hash.
+     *
+     * @param [hash] The hash to verify.
+     * @param [password] The password to verify the hash against.
+     * @return True if the password matches the hash, false otherwise.
+     */
+    fun verify(hash: String?, password: String?): Boolean {
+        if (hash == null || password == null) return false
+        return argon.verify(hash, password.toCharArray())
+    }
+}
