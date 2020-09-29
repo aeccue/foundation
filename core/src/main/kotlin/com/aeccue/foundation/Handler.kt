6346f24c8f274363b8e2863d91efd258fb8738e1
aeccue <develop@aeccue.com>
2020-09-29 23:30:18 +0900

    Created the Handler
    Implementing classes are meant to handle one class using another class.

---
+++ core/src/main/kotlin/com/aeccue/foundation/Handler.kt
@@ -0,0 +1,19 @@
+package com.aeccue.foundation
+
+/**
+ * An interface that dictates that the implementing class is capable of handling an object of class
+ * [T] using an object of class [H].
+ *
+ * @param [T] The class to handle.
+ * @param [H] The class to use for handling.
+ */
+interface Handler<T, H> {
+
+    /**
+     * Handles a target.
+     *
+     * @param [target] The target to handle.
+     * @param [with] What to use for handling.
+     */
+    fun handle(target: T, with: H)
+}
